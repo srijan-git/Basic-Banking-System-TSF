8968f148c0c04d332b2e4b85fa353f5f69bc45b3
srijan-git <srijan.khan20@gmail.com>
2021-09-03 22:44:26 +0530

    Chnages in Config.php

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ $database = "sparks_bank";
 $username = "root";
 $password = "";
 
-// $conn = mysqli_connect('shareddb-x.hosting.stackcp.net', 'shivangi', '12345678@', 'malyaraaj-313537e38d');
 
 $conn = mysqli_connect("$host", "$username", "$password", "$database");
 
